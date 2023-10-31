bb5ad136b7fedc61626adbee9afdae6bb52fb783
Hokimastah <138883093+Hokimastah@users.noreply.github.com>
2023-10-31 09:19:45 +0700

    Nambah kode
    Coba github

---
+++ Belajar koding/Python/Kuliah/Daspro/tugas 1/soal1.1.py
@@ -0,0 +1,21 @@
+n = int(input())
+a = n//250
+b = (n%250)//100
+c = ((n%250)%100)//50
+d = (((n%250)%100)%50)//20
+e = ((((n%250)%100)%50)%20)//10
+f = (((((n%250)%100)%50)%20)%10)//5
+
+an = str(a)
+bn = str(b)
+cn = str(c)
+dn = str(d)
+en = str(e)
+fn = str(f)
+
+print ( an + ' ' + 'lembar 250 ribuan')
+print ( bn + ' ' + 'lembar 100 ribuan')
+print ( cn + ' ' + 'lembar 50 ribuan')
+print ( dn + ' ' + 'lembar 20 ribuan')
+print ( en + ' ' + 'lembar 10 ribuan')
+print ( fn + ' ' + 'lembar 5 ribuan')
---
+++ Belajar koding/Python/Kuliah/Daspro/tugas 4/4.1(AntonMemanggilDirinya).py
@@ -0,0 +1,8 @@
+a = int(input())
+b = [0]
+c = 1
+for i in range (a-1) :
+    b.appenbd(c)
+    c += b[i]
+d = sum(b)*125
+print(d)
